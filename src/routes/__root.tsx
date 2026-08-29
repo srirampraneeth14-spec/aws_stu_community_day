a7b18a0c7ddea153bdb5bbab55750f3534a5d60d
Pediredla Sri Ram Praneeth <srirampraneeth14@gmail.com>
2026-08-29 18:35:06 +0530

    google search icon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -116,6 +116,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
         rel: "stylesheet",
         href: appCss,
       },
+      { rel: "icon", href: "/favicon.ico", sizes: "any" },
       { rel: "icon", href: "/favicon.png", type: "image/png" },
       { rel: "preconnect", href: "https://fonts.googleapis.com" },
       { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin: "anonymous" },
